345ee93c189354d2bac9ab872b6d198e091a7d94
Jeremiah Norris <jjno91@gmail.com>
2020-05-07 15:03:42 -0500

    remove target group outputs

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,3 @@
-output "listener_arn" {
-  description = "https://www.terraform.io/docs/providers/aws/r/lb_listener.html#arn"
-  value       = aws_lb_listener.https.arn
-}
-
-output "target_group_arn" {
-  description = "https://www.terraform.io/docs/providers/aws/r/lb_target_group.html#arn"
-  value       = aws_lb_target_group.this.arn
-}
-
 output "security_group_id" {
   description = "Selenium is unauthenticated so ingress must be restricted to known networks"
   value       = aws_security_group.alb.id
