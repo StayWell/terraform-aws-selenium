50db8facca3a54b2512bd851775616c23f36acc4
Jeremiah Norris <jjno91@gmail.com>
2020-05-11 10:50:46 -0500

    add firefox and opera deployments

---
+++ opera.tf
@@ -0,0 +1,71 @@
+resource "aws_ecs_task_definition" "opera" {
+  family                   = "${var.id}-opera"
+  container_definitions    = jsonencode(local.opera)
+  execution_role_arn       = aws_iam_role.this.arn
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = var.node_cpu
+  memory                   = var.node_memory
+  tags                     = var.tags
+}
+
+resource "aws_ecs_service" "opera" {
+  name                              = "opera"
+  cluster                           = aws_ecs_cluster.this.id
+  task_definition                   = aws_ecs_task_definition.opera.arn
+  desired_count                     = var.opera_replicas
+  launch_type                       = "FARGATE"
+  propagate_tags                    = "SERVICE"
+  tags                              = var.tags
+
+  network_configuration {
+    security_groups = [aws_security_group.ecs.id]
+    subnets         = tolist(var.subnet_ids)
+  }
+}
+
+locals {
+  opera = [
+    {
+      name        = "this"
+      image       = var.opera_image
+      essential   = true
+      environment = concat(local.opera_env, var.environment)
+
+      command = [
+        "/bin/bash",
+        "-c",
+        "export REMOTE_HOST=http://$(curl -s http://169.254.170.2/v2/metadata | jq -r '.Containers[1].Networks[0].IPv4Addresses[0]'):5555 ; /opt/bin/entry_point.sh"
+      ]
+
+      portMappings = [
+        {
+          containerPort = 5555
+        },
+      ]
+
+      logConfiguration = {
+        logDriver = "awslogs"
+
+        options = {
+          awslogs-group         = aws_cloudwatch_log_group.opera.name
+          awslogs-region        = data.aws_region.this.name
+          awslogs-stream-prefix = "ecs"
+        }
+      }
+    }
+  ]
+
+  opera_env = [
+    {
+      name  = "HUB_HOST"
+      value = aws_route53_record.this.name
+    },
+  ]
+}
+
+resource "aws_cloudwatch_log_group" "opera" {
+  name              = "${var.id}-opera"
+  retention_in_days = var.log_retention
+  tags              = var.tags
+}
